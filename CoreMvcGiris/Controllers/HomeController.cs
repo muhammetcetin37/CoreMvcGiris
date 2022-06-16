7b165ceb27226f8d3ca3f664e73f5eafb5fd1052
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-06-16 13:43:03 +0300

    Add project files.

---
+++ CoreMvcGiris/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvcGiris.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public IActionResult Referanslar()
+        {
+            return View();
+        }
+    }
+}
